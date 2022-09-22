0010770e5b0fcf8eb8617d7d74650b9faed71128
David Cash <addictcodercs@gmail.com>
2022-09-22 12:40:10 -0700

    MIgration

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 global using RPGProject.Models;
+using Microsoft.EntityFrameworkCore;
+using RPGProject.Data;
 using RPGProject.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
